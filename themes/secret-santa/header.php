ce19e1c06df9e692fba88aabf53282fafb4eead5
sam.hawkes <sam.hawkes@comvation.com>
2017-12-11 11:59:48 +0100

    Correct links in header.php

--- themes/secret-santa/header.php
+++ themes/secret-santa/header.php
@@ -1,5 +1,5 @@
 <?php if(!isset($_SESSION['loggedin']) && empty($_SESSION['loggedin'])) {
-    echo "<a href='/Access'>Login/ Registrieren</a><br/>";
+    echo "<a href='/Access/'>Login/ Registrieren</a><br/>";
 }
 ?>
-<a href='/Campaign'>Campaign</a>
+<a href='/Campaign/'>Campaign</a>
