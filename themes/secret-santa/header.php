c459ddd79bb84114863c1750a38574967e185870
sam.hawkes <sam.hawkes@comvation.com>
2017-12-14 16:57:30 +0100

    Add css file

--- themes/secret-santa/header.php
+++ themes/secret-santa/header.php
@@ -1,10 +1,11 @@
+<!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Secret Santa Manager</title>
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/themes/secret-santa/css/style.css" type="text/css">
 </head>
 
 <body>
